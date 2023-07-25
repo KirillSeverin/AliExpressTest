e1f9fb56714723ac527ff88e3bd7965f5f895093
Kirill Severin <kirillseverin2020@gmail.com>
2023-07-25 15:00:32 +0300

    Initial Commit

---
+++ AliExpressTest/AliExpressTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AliExpressTestApp.swift
+//  AliExpressTest
+//
+//  Created by Kirill on 25.07.23.
+//
+
+import SwiftUI
+
+@main
+struct AliExpressTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
